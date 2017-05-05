a4702683dd6f7a8954cf8a55ceab8a2efc2548a1
채민균 <coalsrbs7@gmail.com>
2017-05-05 19:01:12 +0900

    Association 추

--- app/models/enrollment/lecture.rb
+++ app/models/enrollment/lecture.rb
@@ -1,4 +1,6 @@
 module Enrollment
   class Lecture < ActiveRecord::Base
+    has_many :users, through: :enrolls
+    has_many :users, through: :lecture_admins
   end
 end
--- app/models/enrollment/lecture_admin.rb
+++ app/models/enrollment/lecture_admin.rb
@@ -1,4 +1,6 @@
 module Enrollment
   class LectureAdmin < ActiveRecord::Base
+    belongs_to :enrollment_lecture, :class_name => 'Enrollment::Lecture'
+    belongs_to :users
   end
 end
