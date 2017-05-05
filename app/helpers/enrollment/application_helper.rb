dc1012f2a5ea3e921748212c7e6b9fc459bb8b60
채민균 <coalsrbs7@gmail.com>
2017-05-05 21:09:12 +0900

    헬퍼메소드 추

--- app/helpers/enrollment/application_helper.rb
+++ app/helpers/enrollment/application_helper.rb
@@ -1,4 +1,36 @@
 module Enrollment
   module ApplicationHelper
+    def current_user
+    end
+
+    def can_enroll?
+
+    end
+
+    def is_lecture_limited?
+
+    end
+
+    def does_lecture_time_limit_end?(lecture_id)
+      begin
+        lecture = Lecture.where(id: lecture_id).first
+        return Time.current > lecture.time_limit_end
+      rescue ActiveRecord::RecordNotFound
+        return nil
+      end
+    end
+
+    def does_lecture_full?(lecture_id)
+      begin
+        lecture = Lecture.where(id: lecture_id).first
+        return lecture.enrollment_enrolls.count >= @lecture.personnel_limit
+      rescue ActiveRecord::RecordNotFound
+        return nil
+      end
+    end
+
+    def is_lecture_admin?(lecture_id, user_id)
+      LectureAdmin.where(lecture_id: lecture_id, user_id: user_id).any?
+    end
   end
 end
