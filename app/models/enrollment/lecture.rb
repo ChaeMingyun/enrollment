7c6a08ab39d7a5b6bd86cd26525d56a4e3458a51
채민균 <coalsrbs7@gmail.com>
2017-05-14 18:53:35 +0900

    모델관계 의존성 추

--- app/models/enrollment/lecture.rb
+++ app/models/enrollment/lecture.rb
@@ -1,7 +1,7 @@
 module Enrollment
   class Lecture < ActiveRecord::Base
     belongs_to :category, foreign_key: :enrollment_category_id
-    has_many :enrolls, foreign_key: :enrollment_lecture_id
-    has_many :lecture_admins, foreign_key: :enrollment_lecture_id
+    has_many :enrolls, foreign_key: :enrollment_lecture_id, dependent: :destroy
+    has_many :lecture_admins, foreign_key: :enrollment_lecture_id, dependent: :destroy
   end
 end
