0e727b9c47de535848293e2b11424950a08252f4
Karoid <shj5508@naver.com>
2017-06-22 17:24:41 +0900

    generating view file added
    the bug of making view file fixed. now working
    lib/enrollmet/generators/enrollment_views

--- lib/generators/enrollment_views/enrollment_views_generator.rb
+++ lib/generators/enrollment_views/enrollment_views_generator.rb
@@ -4,8 +4,12 @@ class EnrollmentViewsGenerator < Rails::Generators::Base
   def views
     empty_directory 'app/views/enrollment'
 
-    Dir.foreach File.expand_path("../../../../app/views/enrollment", __FILE__) do |file| # Searching for files in app/views
-      copy_file(file, 'app/views/enrollment/' + file) if file != '.' && file != '..'
+    Dir.foreach File.expand_path("../../../../app/views/enrollment", __FILE__) do |file1| # Searching for files in app/views
+      Dir.foreach File.expand_path("../../../../app/views/enrollment/"+file1, __FILE__) do |file2|
+        copy_file(file1+"/"+file2, 'app/views/enrollment/' + file1+"/"+file2) if file1 != '.' && file1 != '..' && file2 != '.' && file2 != '..' && file1 != 'layouts'
+      end
     end
+    
   end
+
 end
